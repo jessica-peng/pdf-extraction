76f355b0d9952bf7c20f9492bb17ba7ba7db86e1
Jessica <jessica761116@gmail.com>
2024-05-08 23:05:35 +0800

    fixed mongodb failed

--- frontend/src/app/@core/global.ts
+++ frontend/src/app/@core/global.ts
@@ -1,5 +1,4 @@
 export const GlobalVariable = Object.freeze({
-  BASE_API_URL: "http://127.0.0.1:5002/api/",
+  BASE_API_URL: "http://backend:5002/api/",
   BASE_USER_ID: "62d27f77c33fd8b17cc0ff8d"
 });
-
